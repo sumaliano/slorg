dbf8962ecd36b546d087c259692c2ed367b4de3d
Joel Silva <joel42silva@gmail.com>
2019-06-19 09:54:20 +0200

    test

--- st_0.8.2/editscreen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Open screen content in an editor to copy text. This is alternative to
-# keyboard_select st patch. Set $EDITOR to whatever you want.
-
-tmpfile=$(mktemp /tmp/st-edit.XXXXXX)
-sed -n  p > "$tmpfile"
-st -e nvim "$tmpfile"
-rm "$tmpfile"
